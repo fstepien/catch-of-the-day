d86df97b7980d5dc597e9e61d62b21d4bb0af818
Filip Stepien <contact@filipstepien.com>
2018-03-11 17:55:27 -0400

    first commit

---
+++ src/components/StorePicker.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { getFunName } from "../helpers";
+class StorePicker extends React.Component {
+
+  constructor(){
+    super();
+    this.goToStore = this.goToStore.bind(this);
+  }
+  
+  myInput = React.createRef();
+
+  
+
+  goToStore(e){
+    e.preventDefault();
+    // This only works because it has been bound in the contsctur
+    const storeName = this.myInput.value.value;
+    this.props.history.push(`/store/${storeName}`)
+  }
+
+  render(){
+    return (
+
+      <form action="" className="store-selector" onSubmit={this.goToStore}>
+        <h2>Please Enter A Store</h2>
+        <input type="text" 
+        ref={this.myInput}
+        required placeholder="Store Name" defaultValue={getFunName()} />
+        <button type="submit">Visit Store →</button>
+      </form>
+    )
+  }
+}
+
+export default StorePicker;
---
+++ src/helpers.js
@@ -0,0 +1,68 @@
+export function formatPrice(cents) {
+  return (cents / 100).toLocaleString("en-US", {
+    style: "currency",
+    currency: "USD"
+  });
+}
+
+export function rando(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+export function slugify(text) {
+  return text
+    .toString()
+    .toLowerCase()
+    .replace(/\s+/g, "-")
+    .replace(/[^\w-]+/g, "")
+    .replace(/--+/g, "-")
+    .replace(/^-+/, "")
+    .replace(/-+$/, "");
+}
+
+export function getFunName() {
+  const adjectives = [
+    "adorable",
+    "beautiful",
+    "clean",
+    "drab",
+    "elegant",
+    "fancy",
+    "glamorous",
+    "handsome",
+    "long",
+    "magnificent",
+    "old-fashioned",
+    "plain",
+    "quaint",
+    "sparkling",
+    "ugliest",
+    "unsightly",
+    "angry",
+    "bewildered",
+    "clumsy",
+    "defeated",
+    "embarrassed",
+    "fierce",
+    "grumpy",
+    "helpless",
+    "itchy",
+    "jealous",
+    "lazy",
+    "mysterious",
+    "nervous",
+    "obnoxious",
+    "panicky",
+    "repulsive",
+    "scary",
+    "thoughtless",
+    "uptight",
+    "worried"
+  ];
+
+  const nouns = [
+    "fish"
+  ];
+
+  return `${rando(adjectives)}-${rando(adjectives)}-${rando(nouns)}`;
+}
